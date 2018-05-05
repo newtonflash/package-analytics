841e43b7c56efa1b748a735446f6bf27a22fee47
pradyumna <newton@newtonflash.com>
2018-05-05 13:09:34 +0530

    removed updatePackage method.

--- api/fire-photon/controller.js
+++ api/fire-photon/controller.js
@@ -2,15 +2,11 @@ const mongoService = require('./mongo-service');
 const packageService = require('./mongo-service');
 // const model = require('./schema');
 
-const updateRecords = function (data, responseCB){
-
-};
-
 const addPackage = function addPackage(data, done) {
     packageService.addPackage(data, done);
 };
 
 module.exports = {
-    updateRecords,
+
     addPackage
 };
