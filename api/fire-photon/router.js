be931c33583b7ba12f0628845177e772076b96d2
pradyumna <newton@newtonflash.com>
2018-05-05 13:11:41 +0530

    removed updatePackage method.

--- api/fire-photon/router.js
+++ api/fire-photon/router.js
@@ -2,25 +2,7 @@ const router = require('express').Router();
 const packageController = require('./controller');
 const errorMsg = "Something went wrong behind the scenes.";
 
-router.get('/', function(req, res){
-    try {
-        let reqParams = req.body;
-        packageController.updateRecords(req.body,
-            function (err, result) {
-                if (err) {
-                    console.error('Error in updating records \n', err);
-                    res.status(400).send({error: errorMsg});
-                    return;
-                }
-                res.send(result);
-            });
-    } catch (err) {
-        console.error(errorMsg, err);
-        res.status(500).send({error: 'wrong server'});
-    }
-});
-
-router.post('/fire-photon', (req, res) => {
+router.post('/', (req, res) => {
     try {
         let newPackage = req.body;
         packageController.addPackage(newPackage, (err, result) => {
